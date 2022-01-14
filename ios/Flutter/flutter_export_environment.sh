bc61a4b3fddaa9958f0ce822421c5490285a30ca
chaizafran97 <82008745+chaizafran97@users.noreply.github.com>
2022-01-14 17:45:04 +0800

    .

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Micky\Documents\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Micky\Documents\GitHub\flutter_movie_app"
+export "FLUTTER_ROOT=C:\Users\micky\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\micky\Documents\GitHub\Custom Layout Assignment\mobile-auth"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
